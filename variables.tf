b9c69269a139c9a3900d61c4d9fb48f3aad4c7ae
pharesd <korisophares@gmail.com>
2023-10-25 18:02:53 +0300

    add bucket name variable

--- variables.tf
+++ variables.tf
@@ -3,13 +3,26 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"  # Change to your desired VPC CIDR block
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"  # Change to your desired public subnet CIDR block
+  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  default = "10.0.2.0/24"  # Change to your desired private subnet CIDR block
+  default = "10.0.2.0/24"
+}
+
+# Create S3 buckets
+variable "bucket_name1" {
+  description = "Name for the first S3 bucket"
+  type        = string
+  default     = "my-first-bucket"
+}
+
+variable "bucket_name2" {
+  description = "Name for the second S3 bucket"
+  type        = string
+  default     = "my-second-bucket"
 }
